6cc933e62d7bbe06b961299ac78263dd4762d710
jerrymaloney <gpjerrymaloney@gmail.com>
2014-01-13 04:52:48 +0000

    add test for leptonica -- currently just a placeholder

---
+++ deploy/test-leptonica.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# I can't figure out how to test leptonica directly. If you get an error like 
+# the following when testing tesseract, that probably means leptonica was not 
+# installed correctly.
+# 
+#   Tesseract Open Source OCR Engine v3.01 with Leptonica
+#   Error in pixReadStreamPng: function not present
+#   Error in pixReadStream: png: no pix returned
+#   Error in pixRead: pix not read
+#   Unsupported image type.
