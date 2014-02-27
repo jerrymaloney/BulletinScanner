e090fa3e247a5734ee4f7c5b08709df3845b08a7
jerrymaloney <gpjerrymaloney@gmail.com>
2014-02-27 05:04:28 +0000

    add python packages for NLP processing

--- puppet/modules/bulletinscanner/manifests/init.pp
+++ puppet/modules/bulletinscanner/manifests/init.pp
@@ -5,4 +5,11 @@
 class bulletinscanner {
   include bulletinscanner::params
   include tesseract
+  
+  # install pyton natural language tools
+  # fuck pip, that shit don't work. i'll stick with yum.
+  package { ['numpy', 'PyYAML', 'python-nltk']: 
+    ensure => 'present', 
+  }
+  
 }
